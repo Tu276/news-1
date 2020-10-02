ee9fa0f41d50598c435197efbf2859b8c9578f00
Tu276 <tu276nathan@gmail.com>
2020-10-02 20:44:13 +0300

    add index.css

--- app/views.py
+++ app/views.py
@@ -23,3 +23,15 @@ def movie(movie_id):
     View movie page function that returns the movie details page and its data
     '''
     return render_template('movie.html',id = movie_id)
+
+@app.route('/movie/<int:id>')  # import get_movie function
+
+def movie(id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    movie = get_movie(id)
+    title = f'{movie.title}'
+
+    return render_template('movie.html',title = title,movie = movie)
