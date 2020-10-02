8477fe892c3c7e7c48e1c93362452529088f79b0
Tu276 <tu276nathan@gmail.com>
2020-10-02 21:40:07 +0300

    add wtf forms

--- instance/config.py
+++ instance/config.py
@@ -1,4 +1,5 @@
 MOVIE_API_KEY = '< b32ad606c1192f578c88de271e21a6b7>'
+SECRET_KEY = '<Flask WTF Secret Key>'
 
 # An example request looks like: 
 # https://api.themoviedb.org/3/movie/550?api_key=b32ad606c1192f578c88de271e21a6b7
