ad84d819d9cb2a181662a8c60b814f5d836f619d
Tu276 <tu276nathan@gmail.com>
2020-10-03 07:52:02 +0300

    update config.py

--- config.py
+++ config.py
@@ -1,37 +1,23 @@
 import os
 
-
 class Config:
     '''
     General configuration parent class
     '''
-    MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
-    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
-    SECRET_KEY = os.environ.get('SECRET_KEY')
 
+    NEWS_API_SOURCE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
+    CATG_API_URL = 'https://newsapi.org/v2/top-headlines?country=de&category={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRECT_KEY = os.environ.get('SECRET_KEY')
 
 class ProdConfig(Config):
-    '''
-    Production  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
     pass
 
-
 class DevConfig(Config):
-    '''
-    Development  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-
     DEBUG = True
 
-
 config_options = {
-    'development': DevConfig,
-    'production': ProdConfig
+    'development':DevConfig,
+    'production':ProdConfig
+
 }
