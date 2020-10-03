c4bf1f20342b53ee814826b3b11f4d5d5a92a638
Tu276 <tu276nathan@gmail.com>
2020-10-03 07:43:55 +0300

    add category test

--- instance/config.py
+++ instance/config.py
@@ -1,5 +1,5 @@
-MOVIE_API_KEY = '< b32ad606c1192f578c88de271e21a6b7>'
+MOVIE_API_KEY = '< 0b2c9e06ed484356a1761c783b3fc2f8>'
 SECRET_KEY = '<987654321>'
 
 # An example request looks like: 
-# https://api.themoviedb.org/3/movie/550?api_key=b32ad606c1192f578c88de271e21a6b7
+# https://api.themoviedb.org/3/movie/550?api_key=0b2c9e06ed484356a1761c783b3fc2f8
