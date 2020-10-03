c7c6f8ac29e48bb20947361b1368dc1389137f77
Tu276 <tu276nathan@gmail.com>
2020-10-03 07:44:49 +0300

    add test article changes

--- tests/test_article.py
+++ tests/test_article.py
@@ -1,18 +1,22 @@
 import unittest
-from app.models import movie
-Movie = movie.Movie
+from app.models import Article
 
-class MovieTest(unittest.TestCase):
+class ArticleTest(unittest.TestCase):
     '''
-    Test Class to test the behaviour of the Movie class
+    Test Class to test the behaviour of the Article class
     '''
 
     def setUp(self):
         '''
         Set up method that will run before every Test
         '''
-        self.new_movie = Movie(1234,'Python Must Be Crazy','A thrilling new Python Series','https://image.tmdb.org/t/p/w500/khsjha27hbs',8.5,129993)
+        self.new_article = Article("abc-news-au",
+                                    "ABC News (AU)",
+                                    "Australia's most trusted..."
+                                    "http://www.abc.net.au/news")
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.new_movie,Movie))
-
+        '''
+        Test to check creation of new article instance
+        '''
+        self.assertTrue(isinstance(self.new_article,Article))
