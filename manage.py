106457c1f84322d42a8459291bf70223921b835a
Tu276 <tu276nathan@gmail.com>
2020-10-03 06:33:30 +0300

    add manage.py edits

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-from app import app
+
 from app import create_app
 from flask_script import Manager,Server
 
@@ -7,6 +7,14 @@ app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
+########## unittest###############
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+    ###################
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
