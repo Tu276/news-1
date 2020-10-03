68c60ffb302447bf6692e915eb532949a29378ff
Tu276 <tu276nathan@gmail.com>
2020-10-03 06:27:15 +0300

    add start.sh

--- instance/config.py
+++ instance/config.py
@@ -1,5 +1,5 @@
 MOVIE_API_KEY = '< b32ad606c1192f578c88de271e21a6b7>'
-SECRET_KEY = '<Flask WTF Secret Key>'
+SECRET_KEY = '<987654321>'
 
 # An example request looks like: 
 # https://api.themoviedb.org/3/movie/550?api_key=b32ad606c1192f578c88de271e21a6b7
